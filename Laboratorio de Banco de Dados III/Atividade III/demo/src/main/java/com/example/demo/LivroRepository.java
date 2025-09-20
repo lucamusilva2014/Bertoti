0a11e5f0fd40d0455a200d3e48930f7042cfec87
lucamusilva2014 <lucamusilva2014@gmail.com>
2025-09-19 22:22:07 -0300

    Delete Laboratorio de Banco de Dados III/Atividade III directory

--- Laboratorio de Banco de Dados III/Atividade III/demo/src/main/java/com/example/demo/LivroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import com.example.demo.Livro;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LivroRepository extends CrudRepository<Livro, Long> {
-}
